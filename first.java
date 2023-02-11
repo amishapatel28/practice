07dae0ec16d41a9c9e0f9b52b6c72baaeb92c332
Amisha patel <124456519+amishapatel28@users.noreply.github.com>
2023-02-11 17:03:05 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,5 @@
+class first{
+    public static void main(String arg[]){
+        Syem.out.println("This is my first program");
+    }
+}
---
+++ sub.java
@@ -0,0 +1,8 @@
+class sub{
+    public static void main(String[] args)
+    {
+        int a=4,b=3,s;
+        s=a-b;
+        System.out.println(s);
+    }
+}
